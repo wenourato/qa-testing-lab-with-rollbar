76d25843c9b4d249c3e98d5953264bb1ad45499e
Wyatt Enourato <wenourato@gmail.com>
2021-12-16 14:08:40 -0700

    updated-.ja

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const path = require('path')
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+
+})
+const port = process.env.PORT || 4545
+
+
+
+app.listen(port, () => console.log(`Take us to ward ${port}!`))
